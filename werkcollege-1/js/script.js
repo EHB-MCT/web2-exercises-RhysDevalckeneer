c9ab035e74c482e4b6e655ef4c75bd267e557fc8
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-02 11:13:48 +0200

    Added some new things

--- werkcollege-1/js/script.js
+++ werkcollege-1/js/script.js
@@ -7,14 +7,25 @@ window.onload = function () {
         event.preventDefault();
         console.log('clicked!');
 
+        // Get input values
         let customerName = document.getElementById('name').value;
         let customerEmail = document.getElementById('email').value;
         let customerOrder = document.getElementById('order').value;
+
+        let order = {
+            customerName: customerName,
+            customerEmail: customerEmail,
+            customerOrder: customerOrder,
+            printOrder: function () {
+                return `The order for the customer <strong>${this.customerName}</strong> is the following: <strong>${this.customerOrder}</strong>.<br />The customer may be notified by email: <strong>${this.customerEmail}</strong>`;
+            }
+        }
 
         console.log(customerEmail, customerName, customerOrder);
 
         // Remove form & display order
         document.getElementById("form").style.display = "none";
-        document.getElementById('displayOrder').innerHTML = `The order for the customer <strong>${customerName}</strong> is the following: <strong>${customerOrder}</strong>.<br />The customer may be notified by email: <strong>${customerEmail}</strong>`;
+        document.getElementById('displayOrder').innerHTML = order.printOrder(customerEmail, customerName, customerOrder);
     });
+
 }
