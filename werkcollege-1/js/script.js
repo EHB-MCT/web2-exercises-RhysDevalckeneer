af9f1794a308aadab1a1b0bebff54a617e88ad2c
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-02 22:02:24 +0200

    Added new code

--- werkcollege-1/js/script.js
+++ werkcollege-1/js/script.js
@@ -3,6 +3,34 @@
 window.onload = function () {
     console.log('Window is fully loaded!');
 
+    // Dishes array
+    let dishes = [
+        {
+            "id": 1,
+            "name": "Big Kahuna Burger",
+            "price": 12
+        },
+        {
+            "id": 2,
+            "name": "Royal with Cheese",
+            "price": 8
+        },
+        {
+            "id": 3,
+            "name": "Jack Rabbit Slim's $5 Shake",
+            "price": 5
+        },
+    ];
+
+    // Loop over array data
+    for (let dish of dishes) {
+        console.log(dish);
+
+        document.getElementById('form').insertAdjacentHTML('beforeend', `
+        <input type="radio" id="${dish.id}" name="dish" value="${dish.name}">
+        <label label for= "dish">${dish.name}</label ><br>`);
+    }
+
     document.getElementById('form').addEventListener('submit', event => {
         event.preventDefault();
         console.log('clicked!');
@@ -10,22 +38,19 @@ window.onload = function () {
         // Get input values
         let customerName = document.getElementById('name').value;
         let customerEmail = document.getElementById('email').value;
-        let customerOrder = document.getElementById('order').value;
-
-        let order = {
-            customerName: customerName,
-            customerEmail: customerEmail,
-            customerOrder: customerOrder,
-            printOrder: function () {
-                return `The order for the customer <strong>${this.customerName}</strong> is the following: <strong>${this.customerOrder}</strong>.<br />The customer may be notified by email: <strong>${this.customerEmail}</strong>`;
-            }
-        }
+        // Get value from the checked radio button
+        let customerOrder = document.querySelector('input[name="dish"]:checked').value;
 
         console.log(customerEmail, customerName, customerOrder);
 
+        // Returns order
+        let printOrder = () => {
+            return `The order for the customer <strong>${customerName}</strong> is the following: <strong>${customerOrder}</strong>.<br />The customer may be notified by email: <strong>${customerEmail}</strong>
+            <button id="anotherOrder">Place another order</button>`;
+        }
+
         // Remove form & display order
         document.getElementById("form").style.display = "none";
-        document.getElementById('displayOrder').innerHTML = order.printOrder(customerEmail, customerName, customerOrder);
+        document.getElementById('displayOrder').innerHTML = printOrder(customerEmail, customerName, customerOrder);
     });
-
 }
