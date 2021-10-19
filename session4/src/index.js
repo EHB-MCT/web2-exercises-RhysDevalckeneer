6aa8916ae9ad4a7f7df9fb581d4bdbbf80f69336
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-19 13:11:16 +0200

    Added some changes

--- session4/src/index.js
+++ session4/src/index.js
@@ -1,18 +1,29 @@
+'use strict';
 
 // import Cleave
 // https://www.npmjs.com/package/cleave.js
 import Cleave from 'cleave.js';
+require('cleave.js/dist/addons/cleave-phone.be');
 
 window.onload = function () {
     console.log('Loaded');
+
+    document.getElementById('form').addEventListener('submit', event => {
+        event.preventDefault();
+    
+        let name = document.getElementById('name').value;
+        console.log('Clicked!');
+
+        new Cleave('.name', {
+            phone: true,
+            phoneRegionCode: 'BE'
+        });
+        console.log(name);
+    });
+
+    
+    
 }
 
-document.getElementById('form').addEventListener('submit', event => {
-    event.preventDefault();
-
-    let name = document.getElementById('').value;
-    console.log('Clicked!');
-    console.log(name);
-});
 
 
