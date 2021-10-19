0ad1c250821a07161a6b30661e727cda5d7d6aa6
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-10-19 12:21:17 +0200

    Updated Session 4

--- session4/src/index.js
+++ session4/src/index.js
@@ -1,3 +1,18 @@
-'use strict';
 
-console.log('Loaded!');
+// import Cleave
+// https://www.npmjs.com/package/cleave.js
+import Cleave from 'cleave.js';
+
+window.onload = function () {
+    console.log('Loaded');
+}
+
+document.getElementById('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    let name = document.getElementById('').value;
+    console.log('Clicked!');
+    console.log(name);
+});
+
+
